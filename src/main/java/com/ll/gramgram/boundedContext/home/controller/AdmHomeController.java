4f5318290d5baf18b1840d7648490cadb8803852
KimWonhee626 <kwh7739@gmail.com>
2023-05-02 16:24:34 +0900

    관리자 페이지에 layout 적용, 관리자 페이지에 접속시에는 로그인 폼 노출

--- src/main/java/com/ll/gramgram/boundedContext/home/controller/AdmHomeController.java
+++ src/main/java/com/ll/gramgram/boundedContext/home/controller/AdmHomeController.java
@@ -13,6 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class AdmHomeController {
     @GetMapping("")
+    public String showIndex() {
+        return "redirect:/adm/home/main";
+    }
+
+    @GetMapping("/home/main")
     public String showMain() {
         return "adm/home/main";
     }
