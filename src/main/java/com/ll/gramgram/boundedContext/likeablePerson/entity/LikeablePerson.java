46e36d0cbfffb2a5b7c215173c1d8df17197fe8f
KimWonhee626 <kwh7739@gmail.com>
2023-05-03 13:32:08 +0900

    호감취소, 호감사유 변경 3시간 쿨타임  추가 list페이지 수정

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Entity
@@ -43,7 +44,24 @@ public class LikeablePerson extends BaseEntity {
 
     // 초 단위에서 올림 해주세요.
     public String getModifyUnlockDateRemainStrHuman() {
-        return "2시간 16분";
+        String result = "";
+
+        if(isModifyUnlocked()){
+            return null;
+        }
+        Duration diff = Duration.between(LocalDateTime.now(), modifyUnlockDate);
+
+        long diffHours = diff.toHours();
+        long diffMinutes = diff.toMinutes() - 60 * diffHours;
+
+        if(diffHours == 0){
+            result = diffMinutes + "분";
+        }
+        else{
+            result = diffHours + "시간" + diffMinutes + " 분";
+        }
+
+        return result;
     }
 
     public String getAttractiveTypeDisplayName() {
